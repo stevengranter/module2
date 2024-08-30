c192b76ff179c62d1ad3e5e8752526e24a6b27c5
Steven Granter <stevengranter@gmail.com>
2024-08-30 18:08:19 -0230

    Remove enrichCards utility function, added remote loading to WilderKindCard component

--- src/components/card/WilderKindCard.tsx
+++ src/components/card/WilderKindCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import {
@@ -12,10 +13,8 @@ import {
 } from "@mantine/core";
 import { IconBabyCarriage, IconButterfly, IconEgg } from "@tabler/icons-react";
 
-import { useFetch } from "../../hooks/useFetch.ts";
-import { EnrichedCardType } from "../../models/EnrichedCardType.ts";
-import { SpeciesCardType } from "../../models/SpeciesCardType.ts";
-import { JSON_SERVER_URL } from "../../utils/constants.ts";
+// import { EnrichedCardType } from "../../models/EnrichedCardType.ts";
+import { INAT_API_URL, JSON_SERVER_URL } from "../../utils/constants.ts";
 import styles from "./SpeciesCard.module.css";
 import { SpeciesCardSkeleton } from "./SpeciesCardSkeleton.tsx";
 
@@ -23,31 +22,80 @@ export default function WilderKindCard(props: { cardId?: string }) {
   let cardId;
   const params = useParams();
   params.cardId ? (cardId = params.cardId) : (cardId = props.cardId);
-  const { isLoading, error, data } = useFetch<
-    SpeciesCardType & EnrichedCardType
-  >(`${JSON_SERVER_URL}/cards/${cardId}`);
-  console.log(`rendering WilderKindCard`);
-  console.log(`WilderKindCard data: ${data}`);
+
+  const [localData, setLocalData] = useState();
+  const [remoteData, setRemoteData] = useState();
+  const [isLoadingLocal, setIsLoadingLocal] = useState(true);
+  const [isLoadingRemote, setIsLoadingRemote] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchLocalData = async () => {
+      try {
+        const response = await fetch(`${JSON_SERVER_URL}/cards?id=${cardId}`);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const localCardData = await response.json();
+        if (localCardData) {
+          setLocalData(localCardData[0]);
+        }
+        console.log(localData);
+      } catch (err: Error) {
+        setError(err.message);
+      } finally {
+        setIsLoadingLocal(false);
+      }
+    };
+    fetchLocalData();
+  }, [cardId, localData]);
+
+  useEffect(() => {
+    const fetchRemoteData = async () => {
+      if (localData?.taxon_id) {
+        try {
+          const remoteResponse = await fetch(
+            `${INAT_API_URL}/taxa/${localData.taxon_id}`,
+          );
+          if (!remoteResponse.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const remoteCardData = await remoteResponse.json();
+          setRemoteData(remoteCardData.results[0]);
+        } catch (err: Error) {
+          setError(err.message);
+        } finally {
+          setIsLoadingRemote(false);
+        }
+      }
+    };
+
+    fetchRemoteData();
+  }, [localData]); // Fetch remote data whenever localData changes
+
+  console.log(localData);
 
   error && `Error: ${error.message}`;
 
-  return isLoading ? (
+  // return <h1>test</h1>;
+
+  return isLoadingLocal ? (
     <SpeciesCardSkeleton />
   ) : (
     <>
       <Card
         className={styles["card-front"]}
-        key={data?.id}
+        key={localData?.id}
         shadow="md"
         // p='xl'
         radius="lg"
         withBorder
       >
         <Grid justify="space-between" align="center">
-          {data?.nickname && (
+          {localData?.nickname && (
             <GridCol span={9}>
               <Title order={4} size="h2">
-                {data?.nickname}
+                {localData?.nickname}
               </Title>
             </GridCol>
           )}
@@ -57,13 +105,13 @@ export default function WilderKindCard(props: { cardId?: string }) {
         </Grid>
 
         <Card.Section>
-          {data?.imgSrc ? (
-            <AspectRatio ratio={1 / 1}>
+          {localData?.imgSrc ? (
+            <AspectRatio ratio={1}>
               <Image
                 // radius='lg'
                 className={styles.drop_shadow}
-                src={data.imgSrc}
-                alt={data?.name}
+                src={localData.imgSrc}
+                alt={remoteData?.name}
                 loading="lazy"
               />
             </AspectRatio>
@@ -73,15 +121,15 @@ export default function WilderKindCard(props: { cardId?: string }) {
         </Card.Section>
 
         <Title lineClamp={1} order={2} size="h3">
-          {data?.preferred_common_name}
+          {isLoadingRemote ? "Loading..." : remoteData?.preferred_common_name}
         </Title>
         <Title lineClamp={1} order={3} size="h4">
-          {data?.name}
+          {isLoadingRemote ? "Loading..." : remoteData?.name}
         </Title>
         <SimpleGrid>
-          {data?.current_stage === "egg" && <IconEgg />}
-          {data?.current_stage === "larva" && <IconBabyCarriage />}
-          {data?.current_stage === "adult" && <IconButterfly />}
+          {localData?.current_stage === "egg" && <IconEgg />}
+          {localData?.current_stage === "larva" && <IconBabyCarriage />}
+          {localData?.current_stage === "adult" && <IconButterfly />}
         </SimpleGrid>
       </Card>
     </>
