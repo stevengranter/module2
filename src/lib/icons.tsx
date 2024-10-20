edd3040152de1a9625b9ba0375c1dd446a9afa3e
Steven Granter <stevengranter@gmail.com>
2024-10-20 11:13:35 -0230

    Revised SVG properties to PascalCase

--- src/lib/icons.tsx
+++ src/lib/icons.tsx
@@ -129,9 +129,9 @@ function IconHeart() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="icon icon-tabler icons-tabler-outline icon-tabler-heart"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -165,9 +165,9 @@ function IconMapStar() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="icon icon-tabler icons-tabler-outline icon-tabler-map-star"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
