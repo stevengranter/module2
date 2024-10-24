23d3eefd12543862a5812703d02acb3dc823917d
Steven Granter <stevengranter@gmail.com>
2024-10-24 09:29:33 -0230

    Add conditional rendering to Dashboard for non-guests

--- src/components/dashboard/dashboard__index.tsx
+++ src/components/dashboard/dashboard__index.tsx
@@ -1,11 +1,27 @@
+import { useContext } from "react";
+
 import Collections from "~/components/dashboard/Collections.tsx";
 import Nest from "~/components/dashboard/Nest.tsx";
+import { GuestSessionContext } from "~/features/guest/GuestSessionProvider.tsx";
+import ToggleGuestSessionButton from "~/features/guest/ToggleGuestSessionButton.tsx";
 
 export default function Route__Dashboard() {
+  const { isGuest } = useContext(GuestSessionContext);
+
+  if (!isGuest) {
+    return (
+      <>
+        <p>You must be a guest to add to collections</p>
+        <ToggleGuestSessionButton />
+      </>
+    );
+  }
   return (
-    <>
-      <Nest />
-      <Collections />
-    </>
+    isGuest && (
+      <>
+        <Nest />
+        <Collections />
+      </>
+    )
   );
 }
