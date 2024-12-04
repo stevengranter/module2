c9e6d59de066f83c4848f48b8ebcd522c6aed883
Steven Granter <stevengranter@gmail.com>
2024-12-04 16:25:09 -0330

    Enable logging for production

--- src/dev.ts
+++ src/dev.ts
@@ -1,8 +1,11 @@
 import { useLogger as mantineLogger } from "@mantine/hooks"
 
-export const useLogger =
-  process.env.NODE_ENV !== "production" ? mantineLogger : () => {}
+export const useLogger = mantineLogger
 
-if (process.env.NODE_ENV === "production") {
-  console.log = function () {}
-}
+// TODO: Re-enable below version before posting
+// export const useLogger =
+//   process.env.NODE_ENV !== "production" ? mantineLogger : () => {}
+//
+// if (process.env.NODE_ENV === "production") {
+//   console.log = function () {}
+// }
